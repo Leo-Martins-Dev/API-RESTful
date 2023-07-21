9d65a838261c19658b437681fa8e0d57571c9d42
Leonardo Martins <leonardomartins578@gmail.com>
2023-07-21 17:44:21 -0300

    Refactor

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -1,11 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import mysql from "mysql";
 
 const conexao = mysql.createConnection({
-  host: "localhost",
-  port: "3306",
-  user: "root",
-  password: "1234",
-  database: "db-pizzaria",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 conexao.connect();
